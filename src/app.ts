1990c0619fcff745be0fd38fcce11624488c2a1b
Knat-Dev <dorpele10@gmail.com>
2024-09-05 14:03:29 +0300

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,33 @@
 import P5 from 'p5';
 import {
-    doubleClicked,
-    draw,
-    keyPressed,
-    keyReleased,
-    mouseClicked,
-    mouseDragged,
-    mouseMoved,
-    mousePressed,
-    mouseReleased,
-    setup
+  doubleClicked,
+  draw,
+  keyPressed,
+  keyReleased,
+  mouseClicked,
+  mouseDragged,
+  mouseMoved,
+  mousePressed,
+  mouseReleased,
+  setup,
 } from './sketch';
 
 // main styles
 import './style.scss';
 
-declare global {
-    interface Window {
-        p: P5;
-    }
-}
-
-
 export const initializeSketch = (element: HTMLElement) => {
-    new P5((p: P5) => {
-        window.p = p;
-        console.log('here')
-        // Setup the built-in P5 eventhandlers
-        p.setup = () => setup(p);
-        p.draw = () => draw(p);
-        p.keyPressed = () => keyPressed(p);
-        p.keyReleased = () => keyReleased(p);
-        p.mouseMoved = () => mouseMoved(p);
-        p.mouseDragged = () => mouseDragged(p);
-        p.mousePressed = () => mousePressed(p);
-        p.mouseReleased = () => mouseReleased(p);
-        p.mouseClicked = () => mouseClicked(p);
-        p.doubleClicked = () => doubleClicked(p);
-    }, element);
+  new P5((p: P5) => {
+    p.setup = () => setup(p);
+    p.draw = () => draw(p);
+    p.keyPressed = () => keyPressed(p);
+    p.keyReleased = () => keyReleased(p);
+    p.mouseMoved = () => mouseMoved(p);
+    p.mouseDragged = () => mouseDragged(p);
+    p.mousePressed = () => mousePressed(p);
+    p.mouseReleased = () => mouseReleased(p);
+    p.mouseClicked = () => mouseClicked(p);
+    p.doubleClicked = () => doubleClicked(p);
+  }, element);
 };
 
 initializeSketch(document.getElementById('sketch')!);
